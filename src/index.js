93cf6897d427ce2a40391bc30db701e3d820ce5f
Student <example@example.com>
2019-05-22 08:20:10 -0700

    6:51

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-// import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-import { getFirestore } from 'redux-firestore';
-import { getFirebase } from 'react-redux-firebase'
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 
 
-const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})));
+const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
