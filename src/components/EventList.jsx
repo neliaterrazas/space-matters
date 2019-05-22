52cf602ca39d0c4745c3e9e4d3ce6f47c411b9d1
Student <example@example.com>
2019-05-22 11:16:24 -0700

    complete #20

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import EventSummary from './EventSummary';
-
+import { Link } from 'react-router-dom';
 
 const EventList = ({nextEvents}) => {
   return(
     <div className="event-list section">
       { nextEvents && nextEvents.map(nextEvent => {
         return(
-          <EventSummary nextEvent={nextEvent} key={nextEvent.id} />
+          <Link to={'/project/' + nextEvent.id}>
+            <EventSummary nextEvent={nextEvent} key={nextEvent.id} />
+          </Link>
         )
       })}
     </div>
