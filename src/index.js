7aec68f3c2a7b35ae28fdebb1e1353ebf091c3ca
Student <example@example.com>
2019-05-22 08:08:35 -0700

    withExtraArgument

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 // import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-import { getFirestore } from 'redux-firestore'
+import { getFirestore } from 'redux-firestore';
 import { getFirebase } from 'react-redux-firebase'
 
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
